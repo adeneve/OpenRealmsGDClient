861b9523b4a2faaf0c94a541f582dd44bcabaf2f
Andrew de Neve <adeneve83@gmail.com>
2024-07-18 21:26:27 -0700

    download gltf files via Http server instead

--- Client/Game/JoinServer.gd
+++ Client/Game/JoinServer.gd
@@ -1,37 +1,106 @@
 extends Node
 
 
-@onready var _client: WebSocketClient = $WebSocketClient
-
 var webSocketConnected = false
-var resourcePacketsIncoming = 0
 
 var gltfArray = PackedByteArray()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#connect to ENet server
-	var peer = ENetMultiplayerPeer.new()
-	peer.create_client(Globals.hostname, Globals.ENetport)
-	multiplayer.multiplayer_peer = peer
-	multiplayer.connected_to_server.connect(_on_connected_ok)
-	multiplayer.connection_failed.connect(_on_connected_fail)
-	
-	var err = _client.connect_to_url(Globals.hostname + ":" + str(Globals.WebSocketport))
-	if err != OK:
-		print("Websocket Error connecting to host: %s" % [Globals.hostname])
-		return
-	
-	$HUD/CC/Loading.visible = true
-	await get_tree().create_timer(3).timeout
-	
-	if !webSocketConnected:
-		print("WebSocket connection error")
-		return
-		
-	_client.send("sendresources")
+	#connect to http server
+	#placeholder code.. texture file names should be dynamically provided
+	load_gltf_from_server("/mainScene/textures/aba_rope_baseColor.png")
+	load_gltf_from_server("/mainScene/textures/aba_rope_normal.png")
+	load_gltf_from_server("/mainScene/textures/fern_baseColor.png")
+	load_gltf_from_server("/mainScene/textures/gradient_color_baseColor.png")
+	load_gltf_from_server("/mainScene/textures/grass_baseColor.png")
+	load_gltf_from_server("/mainScene/textures/leave_baseColor.png")
+	load_gltf_from_server("/mainScene/textures/material_baseColor.png")
+	load_gltf_from_server("/mainScene/textures/reed_baseColor.png")
+	load_gltf_from_server("/mainScene/textures/soil_baseColor.png")
+	load_gltf_from_server("/mainScene/scene.bin")
+	load_gltf_from_server("/mainScene/scene.gltf")
+	var gltd = GLTFDocument.new()
+	var gltfs = GLTFState.new()
+	gltd.append_from_file("res://Scenes/mainScene/scene.gltf", gltfs)
+	var node = gltd.generate_scene(gltfs)
+	add_child(node)
+	print("Done")
+	pass
 
+func load_gltf_from_server(path):
+	var err = 0
+	var http = HTTPClient.new() # Create the Client.
 
+	err = http.connect_to_host(Globals.hostname, Globals.WebServerPort) # Connect to host/port.
+	assert(err == OK) # Make sure connection is OK.
+
+	# Wait until resolved and connected.
+	while http.get_status() == HTTPClient.STATUS_CONNECTING or http.get_status() == HTTPClient.STATUS_RESOLVING:
+		http.poll()
+		print("Connecting...")
+		#await get_tree().process_frame
+
+	assert(http.get_status() == HTTPClient.STATUS_CONNECTED) # Check if the connection was made successfully.
+
+	# Some headers
+	var headers = [
+		"User-Agent: Pirulo/1.0 (Godot)",
+		"Accept: */*"
+	]
+
+	err = http.request(HTTPClient.METHOD_GET, path, headers) # Request a page from the site (this one was chunked..)
+	assert(err == OK) # Make sure all is OK.
+
+	while http.get_status() == HTTPClient.STATUS_REQUESTING:
+		# Keep polling for as long as the request is being processed.
+		http.poll()
+		print("Requesting...")
+		#await get_tree().process_frame
+
+	assert(http.get_status() == HTTPClient.STATUS_BODY or http.get_status() == HTTPClient.STATUS_CONNECTED) # Make sure request finished well.
+
+	print("response? ", http.has_response()) # Site might not have a response.
+
+	if http.has_response():
+		# If there is a response...
+
+		headers = http.get_response_headers_as_dictionary() # Get response headers.
+		print("code: ", http.get_response_code()) # Show response code.
+		print("**headers:\\n", headers) # Show headers.
+
+		# Getting the HTTP Body
+
+		if http.is_response_chunked():
+			# Does it use chunks?
+			print("Response is Chunked!")
+		else:
+			# Or just plain Content-Length
+			var bl = http.get_response_body_length()
+			print("Response Length: ", bl)
+
+		# This method works for both anyway
+
+		var rb = PackedByteArray() # Array that will hold the data.
+
+		while http.get_status() == HTTPClient.STATUS_BODY:
+			# While there is body left to be read
+			http.poll()
+			# Get a chunk.
+			var chunk = http.read_response_body_chunk()
+			if chunk.size() == 0:
+				var a = 5
+				#await get_tree().process_frame
+			else:
+				rb = rb + chunk # Append to read buffer.
+		# Done!
+
+		print("bytes got: ", rb.size())
+		var text = rb.get_string_from_ascii()
+		#print(text)
+		#store byte array as file 'scene.gltf'
+		save("res://Scenes"+path, rb)
+		http.close()
 
 func build_GLTF_Resource(byteArray):
 	var glfd = GLTFDocument.new()
@@ -51,40 +120,20 @@ func build_GLTF_Resource(byteArray):
 func _process(delta):
 	pass
 
-func _on_connected_ok():
-	print("connected to ENet server")
-	pass
 
-func _on_connected_fail():
-	print("failed to connect to ENet server")
-	pass
+func save(path, content):
+	var file = FileAccess.open(path, FileAccess.WRITE)
+	file.store_buffer(content)
 
-
-func _on_web_socket_client_connected_to_server():
-	print("connected to Websocket server")
-	webSocketConnected = true
-	pass # Replace with function body.
-
-func _on_web_socket_client_message_received(message):
-	#first recieve, message packets number X
-	#then read X buffers from server, and reconstruct
-	
-	if typeof(message) != TYPE_STRING:
-		#is probably a resource file
-		if resourcePacketsIncoming > 0:
-			gltfArray.append_array(message)
-			resourcePacketsIncoming -= 1
-			print(resourcePacketsIncoming)
-			print(gltfArray.size())
-		#var decompressed = message.decompress_dynamic(-1)
-		if resourcePacketsIncoming <= 0:
-			build_GLTF_Resource(gltfArray)
-
-	else:
-		if "packets" in message:
-			var split = message.split(" ")
-			resourcePacketsIncoming = int(split[1])
-			print("blah")
-			print(resourcePacketsIncoming)
-			
+func addCollisionMesh(node):
+	for N in node.get_children():
+		if N.get_child_count() > 0:
+			print("["+N.get_name()+"]")
+			addCollisionMesh(N)
+		else:
+			# Do something
+			print("- "+N.get_name()+" type: " + N.get_class())
+			if N.get_class() == "MeshInstance3D":
+				var a = N as MeshInstance3D
+				a.create_trimesh_collision()
 
--- Client/StartMenu/SubMenus/JoinServer/JoinServer.gd
+++ Client/StartMenu/SubMenus/JoinServer/JoinServer.gd
@@ -11,11 +11,6 @@ var GamePath = "res://Game/JoinServer.tscn"
 @onready var _host = $CenterContainer/VBoxContainer/ServerAddress
 @onready var _port = $CenterContainer/VBoxContainer/Port
 
-@onready var _client: WebSocketClient = $WebSocketClient
-
-var webSocketConnectionVerified = false
-var ENetConnectionVerified = false
-var RSAPubKeyVerified = false
 
 @onready var pubkey: String = ""
 @onready var prvkey: String = ""
@@ -27,25 +22,9 @@ func info(msg):
 func UI_info(msg):
 	$CenterContainer/VBoxContainer/InfoText.text = msg
 
-# Client signals
-func _on_web_socket_client_connection_closed():
-	var ws = _client.get_socket()
-	info("Client just disconnected with code: %s, reason: %s" % [ws.get_close_code(), ws.get_close_reason()])
-	webSocketConnectionVerified = false
 
-
-func _on_web_socket_client_connected_to_server():
-	info("Client just connected with protocol: %s" % _client.get_socket().get_selected_protocol())
-	webSocketConnectionVerified = true
-
-
-func _on_web_socket_client_message_received(message):
-	info("%s" % message)
-	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	multiplayer.connected_to_server.connect(_on_connected_ok)
-	multiplayer.connection_failed.connect(_on_connected_fail)
 	pass # Replace with function body.
 
 
@@ -60,134 +39,54 @@ func load_file(path):
 	var content = file.get_as_text()
 	return content
 
-
-@rpc("authority", "call_remote", "reliable")
-func RSAPubKeyIsValid(valid):
-	#decrypt on client side
-	#and finally verify on server side
-	if valid:
-		RSAPubKeyVerified = true
-	else:
-		RSAPubKeyVerified = false
-	
-@rpc("authority", "call_remote", "reliable")
-func sendEncryptedInt(eint):
-	#decrypt on client side
-	#and finally verify on server side
-	print("recieved encrypted int")
-	print(eint)
-	#now decrypt it with prvKey
-	var pk = CryptoKey.new()
-	pk.load_from_string(prvkey, false)
-	var c = Crypto.new()
-	var dint = c.decrypt(pk, eint)
-	var sint = dint.get_string_from_utf8()
-	print(sint)
-	# now send it back to complete verfication
-	sendDecryptedInt.rpc_id(1, int(sint))
-
-
-# Call local is required if the server is also a player.
-@rpc("any_peer", "call_remote", "reliable")
-func sendRSApubkey(publicKey):
-	# invoke on server side
-	pass
-	
-@rpc("any_peer", "call_remote", "reliable")
-func sendDecryptedInt(dint):
-	# invoked on server side
-	pass
-
 	
 func _on_connect_pressed():
-	webSocketConnectionVerified = false
-	ENetConnectionVerified = false
-	# check if RSA key is present
-	print(pubKeyPath)
-	pubkey = load_file(pubKeyPath)
-	prvkey = load_file(prvKeyPath)
-	if pubkey == null or prvkey == null:
-		UI_info("No RSA public/private key found, please go to 'Create Account' to generate an RSA public/private key pair")
-		return
-	print("RSA public/private key found")
-	
+	# test http client connection
 	#1. test connection to WebSocketServer
-	UI_info("attempting to connect to WebSocket Server...")
+	UI_info("attempting to connect to Http Server...")
 	if _host.text == "" or _port.text == "":
 		UI_info("Please specify both server name and port")
 		return
 	info("Connecting to host: %s." % [_host.text])
 	#use port for Enet, and port+1 for WebSocket
-	var webSocketPort = int(_port.text)+1
-	var err = _client.connect_to_url(_host.text + ":" + str(webSocketPort))
+	var err = 0
+	var http = HTTPClient.new() # Create the Client.
+	
+	err = http.connect_to_host(_host.text, int(_port.text)) # Connect to host/port.
+
 	if err != OK:
 		info("Error connecting to host: %s" % [_host.text])
 		return
 	
-	await get_tree().create_timer(3).timeout
+		# Wait until resolved and connected.
+	while http.get_status() == HTTPClient.STATUS_CONNECTING or http.get_status() == HTTPClient.STATUS_RESOLVING:
+		http.poll()
+		print("Connecting...")
+		await get_tree().process_frame
+		
+	print(http.get_status())
+	assert(http.get_status() == HTTPClient.STATUS_CONNECTED) # Check if the connection was made successfully.
 	
-	if webSocketConnectionVerified:
-		UI_info("Successfully connected to WebSocket Server")
-	else:
-		UI_info("failed to connect to WebSocket server")
-		return
+	UI_info("Successfully connected to http Server")
 	
 	Globals.hostname = _host.text 
-	Globals.ENetport = int(_port.text)
-	Globals.WebSocketport = webSocketPort
+	Globals.WebServerPort = _port.text
+	Globals.SocketIOPort = 0 #placeholder
 	
 	print(Globals.hostname)
-	#2. test connection to ENet Server
-	# Create client.
-
-	UI_info("attempting to connect to ENet Server...")
-	var peer = ENetMultiplayerPeer.new()
-	peer.create_client(_host.text, Globals.ENetport)
-	multiplayer.multiplayer_peer = peer
+	
+	#TODO
+	#UI_info("attempting to connect to socketIO Server...")
+	#TODO
 	
 	await get_tree().create_timer(3).timeout
 	
-	if ENetConnectionVerified:
-		UI_info("Successfully connected to ENet Server")
-	else:
-		UI_info("failed to connect to ENet server")
-		return
-	
-	UI_info("verifying RSA public key...")
-	#3. verify RSA key with ENet RPCs
-	sendRSApubkey.rpc_id(1, pubkey) # Send the input only to the server.
-	
-	#wait for RSA verification
-	#~ 5 seconds
-	await get_tree().create_timer(3).timeout
-	
-	if RSAPubKeyVerified:
-		UI_info("Successfully validated RSA public key")
-	else:
-		UI_info("failed to validate RSA public key")
-		return
-	
-	#4. close connections
-	multiplayer.multiplayer_peer = null
-	_client.close()
+	http.close()
 	
 	#5. switch to empty 3D scene, and reconnect in that scene with globals
 	get_tree().change_scene_to_file(GamePath)
 	
-	#6. download required assets from server (WebSocket) if not already cached
-	 # during this step, you can use checksums to
-	# verify that the file was reliably sent
-	# build file in memory, then use FileAcees
-	# to save the file to Godot folder
-	# may be able to send large files
-	# without fragmenting if websocket allows it
-	# try this first
-	# should glb be downloaded, or
 	
-	#7. build world from assets
-
-	# Websocket looks like a good choice for file transfers
-
 	pass # Replace with function body.
 
 
@@ -195,10 +94,4 @@ func _on_back_button_pressed():
 	get_tree().change_scene_to_file(StartMenuPath)
 	pass # Replace with function body.
 	
-func _on_connected_ok():
-	ENetConnectionVerified = true
-	pass
 
-func _on_connected_fail():
-	ENetConnectionVerified = false
-	pass
